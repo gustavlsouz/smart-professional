2204a24efc47e2399152c58c9a963f0bf9fc8315
Gustavo Nunes <gustavolevi.nunes@gmail.com>
2020-03-16 15:11:54 -0300

    format logs

--- src/crosscut/wrappers/Logger.js
+++ src/crosscut/wrappers/Logger.js
@@ -1,14 +1,22 @@
 const winston = require('winston')
 const uuid = require('uuid')
+const { combine, prettyPrint, json, timestamp } = winston.format
 
+const transports = [
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error', maxsize: 1000000 }),
+    new winston.transports.File({ filename: 'logs/combined.log', maxsize: 1000000 }),
+    new winston.transports.Console(),
+]
+const getFormat = () => {
+    if (process.env.NODE_ENV === 'production') {
+        return combine(json(), timestamp())
+    }
+    return combine(json(), timestamp(), prettyPrint())
+}
 const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.json(),
-    transports: [
-        new winston.transports.File({ filename: 'logs/error.log', level: 'error', maxsize: 1000000 }),
-        new winston.transports.File({ filename: 'logs/combined.log', maxsize: 1000000 }),
-        new winston.transports.Console(),
-    ]
+    format: getFormat(),
+    transports,
 });
 
 module.exports = class Logger {
@@ -24,7 +32,6 @@ module.exports = class Logger {
     callLogger({ method = 'info', content, metadata }) {
         this.logger[method]({
             signature: this.signature,
-            logAt: new Date(),
             content,
             metadata,
         })
